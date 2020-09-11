656b3403e089cfdcc99392acdd56dccf9bf98037
Gavin Quirk <gavinquirk@gmail.com>
2020-09-11 14:44:45 -0700

    successfully get login

--- admin_logins.py
+++ admin_logins.py
@@ -3,12 +3,12 @@ import socket
 import itertools
 import json
 
-
 host = sys.argv[1]
 port = int(sys.argv[2])
 
 common_logins = [
-    login.rstrip() for login in list(open("logins.txt").readlines())]
+    login.rstrip() for login in
+    list(open("F:/code/Password Hacker/Password Hacker/task/hacking/logins.txt").readlines())]
 
 client_socket = socket.socket()
 address = (host, port)
@@ -17,17 +17,23 @@ state = False
 
 client_socket.connect(address)
 
-# Try every common login until wrong password response
-for login in common_logins:
-    data = json.dumps({
-        "login": login,
-        "password": ""
-    })
-    client_socket.send(data)
-    response = client_socket.recv(1024).decode()
-    if response == {"result": "Wrong password!"}:
-        admin_login = login
-        print('Admin login is: ' + admin_login)
+
+def get_login():
+    # For every login in the list, send to server as json data with empty password. When response is Wrong Password!
+    # return that login.
+    try:
+        for login in common_logins:
+            data = json.dumps({
+                "login": login,
+                "password": " "
+            })
+            client_socket.send(data.encode())
+            response = json.loads(client_socket.recv(
+                1024).decode(encoding='utf-8'))
+            if response == {"result": "Wrong password!"}:
+                return login
+    except Exception:
+        print("Exception occurred")
 
 
 client_socket.close()
